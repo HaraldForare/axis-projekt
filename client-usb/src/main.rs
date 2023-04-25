dc76c55ea1660cffa9a3799d5431a46a8dd71c8b
emillantz <emil.lantz@gmail.com>
2023-04-25 13:58:57 +0200

    Added logging + fixed clippy warnings

--- client-usb/src/main.rs
+++ client-usb/src/main.rs
@@ -1,21 +1,20 @@
 use dotenv::dotenv;
-use serialport;
 use std::{
     io::{BufRead, BufReader},
     str,
     time::Duration,
 };
-
+mod util;
 use crate::util::camera;
 
-mod util;
 fn main() {
-    dotenv().ok();
-    const BAUD_RATE: u32 = 115200;
+    const BAUD_RATE: u32 = 115_200;
     let ports = serialport::available_ports().expect("No ports!");
     let mut found = false;
     let mut port = None;
     let camera = camera::Camera::new();
+
+    dotenv().ok();
 
     for p in ports {
         println!("{}", p.port_name);
@@ -23,7 +22,7 @@ fn main() {
             println!("USB bus found on {}", p.port_name);
             port = Some(
                 serialport::new(&p.port_name, BAUD_RATE)
-                    .timeout(Duration::from_millis(50))
+                    .timeout(Duration::from_millis(20))
                     .open()
                     .unwrap(),
             );
@@ -31,9 +30,7 @@ fn main() {
             found = true;
         }
     }
-    if !found {
-        panic!("No USB bus found!")
-    }
+    assert!(found, "No USB bus found!");
 
     if let Some(serial) = port {
         let mut reader = BufReader::new(serial);
@@ -41,23 +38,33 @@ fn main() {
             let mut buf = String::new();
             match reader.read_line(&mut buf) {
                 Ok(_t) => {
-                    camera.pan(parse_angle(buf.as_bytes()));
+                    if let Some(angle) = parse_angle(buf.as_bytes()) {
+                        camera.pan(angle);
+                    } else {
+                        println!("Invalid angle");
+                    }
                 }
                 Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
-                Err(e) => eprintln!("{:?}", e),
+                Err(e) => eprintln!("{e:?}"),
             }
         }
     }
 }
 
-fn parse_angle(buf: &[u8]) -> i16 {
+fn parse_angle(buf: &[u8]) -> Option<i16> {
     let binding = buf.to_vec();
+    let angle: i16;
+
     let string = str::from_utf8(&binding)
-        .unwrap_or("0")
+        .unwrap_or("Invalid UTF-8 sequence")
         .trim_matches(|c| c == '\r' || c == '\n');
-    println!("string: {}", &string);
-    let angle = string.parse().unwrap_or(0.0_f32).to_degrees().round() as i16;
-    std::thread::sleep(std::time::Duration::from_millis(100));
-    println!("angle: {} \n", &angle);
-    angle
+
+    if let Ok(n) = string.parse::<f32>() {
+        angle = n.to_degrees().round() as i16;
+        println!("Angle: {angle}");
+    } else {
+        println!("{string}");
+        return None;
+    }
+    Some(angle)
 }
