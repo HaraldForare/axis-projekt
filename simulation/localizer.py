d7ac8e596282a403177838e01784be3486bec932
HaraldForare <harald.forare@gmail.com>
2023-05-04 10:01:21 +0200

    stopped mutating function parameter

--- simulation/localizer.py
+++ simulation/localizer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def localize(mic_positions, arrival_times):
-    arrival_times -= np.mean(arrival_times)
+    arrival_times = arrival_times - np.mean(arrival_times)
     source_direction = -np.sum(np.column_stack((arrival_times, arrival_times)) * mic_positions, axis=0)
     source_direction /= np.linalg.norm(source_direction)
     return source_direction
